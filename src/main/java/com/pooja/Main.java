bd908b28000e6355bfa84ba55ea821a258c7b204
poojasree06 <poojasree.ambati@gmail.com>
2025-03-01 14:50:06 +0530

    Initial commit

---
+++ src/main/java/com/pooja/Main.java
@@ -0,0 +1,9 @@
+package com.pooja;
+
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins!");
+    }
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,11 @@
+package com.pooja;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MainTest {
+    @Test
+    public void testApp() {
+        assertEquals(2, 1 + 1, "Basic Math Test");
+    }
+}
